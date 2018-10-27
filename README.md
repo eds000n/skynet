07a012c2b969063f1fe5b5e8a858603ca32e490c
Edson Ticona <xcr33d@gmail.com>
2018-10-27 09:28:23 +0200

    Added Levenshtein distance implementation and ajax file upload

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SkyNET project
 
 Don't worry, this is not a dangerous project
+
+
+# Requirements
+
+https://github.com/toastdriven/pylev
--- skynet_web/web/views.py
+++ skynet_web/web/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
+import pylev
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,27 @@ def index(request):
 
 def runAlgs(request):
     context = {}
-    return JsonResponse({ 'response': 'res' })
+    matches = levenshtein()
+    return JsonResponse({ 'response': matches })
+
+#async def levenshtein():
+def levenshtein():
+    input_dataset = "microsft,google,facebook"
+    internal_dataset = "microsoft,google,facebook"
+
+    counter = 0
+    input_ds_array = input_dataset.split(",")
+    for input_ds in input_ds_array:
+        minimum=100000
+        for internal_ds in internal_dataset.split(","):
+            t=pylev.damerau_levenshtein(input_ds, internal_ds) 
+            if t<minimum:
+                minimum=t
+            if minimum==0:
+                break
+        if minimum<=2:
+            counter += 1
+    #print "% of matches"
+    #print counter*1.0/len(input_ds_array)
+    return counter*1.0/len(input_ds_array)
+
